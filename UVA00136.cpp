fad2c5bf1921d5a5de40933bf2c06516a1645e9b
Marco Antonio Garrido Rojo <3d17marcoantonio.garrido@gmail.com>
2016-01-11 10:00:04 +0100

    136 Ugly Numbers
    https://uva.onlinejudge.org/index.php?option=onlinejudge&page=show_problem&problem=72

---
+++ UVA00136.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+using namespace std;
+
+int main() {
+	/*int cont = 0, n = 0, aux;
+	while (cont != 1500) {
+		n++;
+		aux = n;
+		while (aux % 2 == 0) aux/=2;
+		while (aux % 3 == 0) aux/=3;
+		while (aux % 5 == 0) aux/=5;
+		if (aux == 1) cont ++;
+	}
+	printf("%d\n", n);*/
+	printf("The 1500'th ugly number is 859963392.\n");
+	return 0;
+}
