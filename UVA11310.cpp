bb00c47fac6acf9eb5799929f631a37fb7cc968f
Marco Antonio Garrido Rojo <3d17marcoantonio.garrido@gmail.com>
2017-04-05 10:00:04 +0200

    11310 Delivery Debacle
    https://uva.onlinejudge.org/index.php?option=onlinejudge&page=show_problem&problem=2285

---
+++ UVA11310.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+long long dp[41];
+
+int main() {
+    dp[0] = 1, dp[1] = 1;
+    for (int i = 2; i < 41; i++) {
+        dp[i] = dp[i-1] + 4*dp[i-2] + 2*dp[i-3];
+    }
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        printf("%lld\n", dp[n]);
+    }
+    return 0;
+}
